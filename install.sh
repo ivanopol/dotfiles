863b1d3dbbad2a4b2bad0e26d0282b9de7cf6afb
ivanopol <ivanopol777@mail.ru>
2019-03-07 12:00:59 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,15 @@
 
 # Get current dir (so run this script from anywhere)
 
-export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
+export DOTFILES_DIR DOTFILES_CACHE 
+
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
-DOTFILES_EXTRA_DIR="$HOME/.extra"
 
-# Make utilities available
 
-PATH="$DOTFILES_DIR/bin:$PATH"
+# Common functions
+
+. "$DOTFILES_DIR/system/.function"
 
 # Update dotfiles itself first
 if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
-
-# Bunch of symlinks
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-
